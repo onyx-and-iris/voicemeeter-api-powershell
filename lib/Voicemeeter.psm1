fce4c51c563c0aaf052a327a120a3ab1fac5b787
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-04-30 17:07:09 +0100

    update to voicemeeter root script
    now using psm1

--- lib/Voicemeeter.psm1
+++ lib/Voicemeeter.psm1
@@ -13,7 +13,7 @@ class Remote {
         $this.Setup()
     }
 
-    [void] Setup () {
+    [void] Setup() {
         Login -TYPE $this.type
 
         $this.button = Buttons
@@ -21,11 +21,25 @@ class Remote {
         $this.bus = Buses
     }
 
-    [void] Logout () {
+    [void] Logout() {
         Logout
     }
-
+    
     [void] Set_Multi([HashTable]$hash) {
         Param_Set_Multi -HASH $hash
     }
 }
+
+Function Get-RemoteBasic {
+    return [Remote]::new('basic')
+}
+
+Function Get-RemoteBanana {
+    return [Remote]::new('banana')
+}
+
+Function Get-RemotePotato {
+    return [Remote]::new('potato')
+}
+
+Export-ModuleMember -Function Get-RemoteBasic, Get-RemoteBanana, Get-RemotePotato
