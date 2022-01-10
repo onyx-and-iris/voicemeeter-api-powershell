093cfe0e855ba073e92c257b4db70ecbdd8090c9
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-01-10 20:56:40 +0000

    Update base.ps1
    add version, p_in, v_in to $layout

--- lib/base.ps1
+++ lib/base.ps1
@@ -221,26 +221,35 @@ Function DefineVersion {
 
     if($TYPE -eq 1) {
         $layout = @{
-           "strip" = 3
-           "bus" = 2
-           "vban_in" = 4
-           "vban_out" = 4
+            "version" = "basic"
+            "p_in" = 2
+            "v_in" = 1
+            "p_out" = 1
+            "v_out" = 1
+            "vban_in" = 4
+            "vban_out" = 4
         }
     }
     elseif($TYPE -eq 2) {
         $layout = @{
-           "strip" = 5
-           "bus" = 5
-           "vban_in" = 8
-           "vban_out" = 8
+            "version" = "banana"
+            "p_in" = 3
+            "v_in" = 2
+            "p_out" = 3
+            "v_out" = 2
+            "vban_in" = 8
+            "vban_out" = 8
         }
     }
     elseif($TYPE -eq 3) {
         $layout = @{
-           "strip" = 8
-           "bus" = 8
-           "vban_in" = 8
-           "vban_out" = 8
+            "version" = "potato"
+            "p_in" = 5
+            "v_in" = 3
+            "p_out" = 5
+            "v_out" = 3
+            "vban_in" = 8
+            "vban_out" = 8
         }
     }
     $global:layout = $layout
