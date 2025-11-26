ee85d5ffd8b4c8c2a4e941305e34d75bf3b72a18
pblivingston <71585805+pblivingston@users.noreply.github.com>
2025-11-26 17:23:01 -0500

    decouple device
    - basic A2 device supported
    - asio only added to bus[0].device

--- lib/bus.ps1
+++ lib/bus.ps1
@@ -101,75 +101,13 @@ class PhysicalBus : Bus {
     }
 }
 
-class BusDevice : IRemote {
-    BusDevice ([int]$index, [Object]$remote) : base ($index, $remote) {
-    }
-
-    [string] identifier () {
-        return 'Bus[' + $this.index + '].Device'
-    }
-
-    hidden $_name = $($this | Add-Member ScriptProperty 'name' `
-        {
-            $this.Getter_String('name')
-        } `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).name is read only")
-        }
-    )
-
-    hidden $_sr = $($this | Add-Member ScriptProperty 'sr' `
-        {
-            $this.Getter('sr')
-        } `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).sr is read only")
-        }
-    )
-
-    hidden $_wdm = $($this | Add-Member ScriptProperty 'wdm' `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).wdm is write only")
-        } `
-        {
-            param($arg)
-            return $this.Setter('wdm', $arg)
-        }
-    )
-
-    hidden $_ks = $($this | Add-Member ScriptProperty 'ks' `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).ks is write only")
-        } `
-        {
-            param($arg)
-            return $this.Setter('ks', $arg)
-        }
-    )
-
-    hidden $_mme = $($this | Add-Member ScriptProperty 'mme' `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).mme is write only")
-        } `
-        {
-            param($arg)
-            return $this.Setter('mme', $arg)
-        }
-    )
-
-    hidden $_asio = $($this | Add-Member ScriptProperty 'asio' `
-        {
-            return Write-Warning ("ERROR: $($this.identifier()).asio is write only")
-        } `
-        {
-            param($arg)
-            return $this.Setter('asio', $arg)
-        }
-    )
-}
-
 class VirtualBus : Bus {
+    [Object]$device
+    
     VirtualBus ([int]$index, [Object]$remote) : base ($index, $remote) {
+        if ($this.remote.kind.name -eq 'basic') {
+            $this.device = [BusDevice]::new($index, $remote)
+        }
     }
 }
 
---
+++ lib/device.ps1
@@ -0,0 +1,83 @@
+class Device : IRemote {
+    Device ([int]$index, [Object]$remote) : base ($index, $remote) {
+    }
+
+    hidden $_name = $($this | Add-Member ScriptProperty 'name' `
+        {
+            $this.Getter_String('name')
+        } `
+        {
+            return Write-Warning ("ERROR: $($this.identifier()).name is read only")
+        }
+    )
+
+    hidden $_sr = $($this | Add-Member ScriptProperty 'sr' `
+        {
+            $this.Getter('sr')
+        } `
+        {
+            return Write-Warning ("ERROR: $($this.identifier()).sr is read only")
+        }
+    )
+
+    hidden $_wdm = $($this | Add-Member ScriptProperty 'wdm' `
+        {
+            return Write-Warning ("ERROR: $($this.identifier()).wdm is write only")
+        } `
+        {
+            param($arg)
+            return $this.Setter('wdm', $arg)
+        }
+    )
+
+    hidden $_ks = $($this | Add-Member ScriptProperty 'ks' `
+        {
+            return Write-Warning ("ERROR: $($this.identifier()).ks is write only")
+        } `
+        {
+            param($arg)
+            return $this.Setter('ks', $arg)
+        }
+    )
+
+    hidden $_mme = $($this | Add-Member ScriptProperty 'mme' `
+        {
+            return Write-Warning ("ERROR: $($this.identifier()).mme is write only")
+        } `
+        {
+            param($arg)
+            return $this.Setter('mme', $arg)
+        }
+    )
+}
+
+class BusDevice : Device {
+    BusDevice ([int]$index, [Object]$remote) : base ($index, $remote) {
+        if ($this.index -eq 0) {
+            $this.AddASIO()
+        }
+    }
+
+    [string] identifier () {
+        return 'Bus[' + $this.index + '].Device'
+    }
+
+    hidden [void] AddASIO () {
+        Add-Member -InputObject $this -MemberType ScriptProperty -Name 'asio' `
+            -Value {
+            return Write-Warning ("ERROR: $($this.identifier()).asio is write only")
+        } -SecondValue {
+            param($arg)
+            return $this.Setter('asio', $arg)
+        } -Force
+    }
+}
+
+class StripDevice : Device {
+    StripDevice ([int]$index, [Object]$remote) : base ($index, $remote) {
+    }
+
+    [string] identifier () {
+        return 'Strip[' + $this.index + '].Device'
+    }
+}
